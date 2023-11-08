ef710056435923bbfe05b48cbcb6d7b7628a904b
Mohdhussain28 <mohdhussain28@outlook.com>
2023-11-08 14:39:32 +0530

    remove .env file

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "../.env" });
 
 const connectDatabase = () => {
-    mongoose.connect('mongodb+srv://md28mdhussain:nminruwO2FoQUdHV@cluster0.cokvi3i.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'MongoDB connection error:'));
     db.once('open', () => {
